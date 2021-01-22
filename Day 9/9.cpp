b8268dfc25aa930f9ef18e03c2a5693ccd12e8e2
Muhid Hassan Risvy <50704245+Risvy@users.noreply.github.com>
2021-01-23 01:15:12 +0600

    Create 9.cpp

---
+++ Day 9/9.cpp
@@ -0,0 +1,13 @@
+/*
+https://leetcode.com/problems/check-if-it-is-a-good-array/
+Tags: Dynamic Array, Hard 
+*/
+
+
+
+class Solution:
+    def isGoodArray(self, nums: List[int]) -> bool:
+        if gcd(*nums)==1:
+            return True
+        return False
+        
