4ac476ae00919a47137094a948188e625f7b65bc
Muhid Hassan Risvy <50704245+Risvy@users.noreply.github.com>
2021-01-23 22:39:59 +0600

    Create 10.cpp

---
+++ Day 10/10.cpp
@@ -0,0 +1,15 @@
+/*
+https://leetcode.com/problems/binary-search/
+Tags: Binary Search, Easy
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++)
+            if(target==nums[i]) return i;
+        return -1;
+        
+    }
+};
+
