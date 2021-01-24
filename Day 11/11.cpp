066c55cc87087e8f85b50979967e3300b346fe78
Muhid Hassan Risvy <50704245+Risvy@users.noreply.github.com>
2021-01-24 21:54:44 +0600

    Create 11.cpp

---
+++ Day 11/11.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/search-insert-position/
+Tags: Binary Search, Easy
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        for(int i=0; i<nums.size();i++)
+        {
+            if(target==nums[i]) return i;
+        }
+        for(int i=0; i<nums.size()-1;i++)
+        {
+            if(target>nums[i] and target<nums[i+1]) return i+1;
+        }
+        if(target<nums[0]) return 0;
+        return nums.size();
+    }
+};
