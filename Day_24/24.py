200710d11484b0659515233491115194419b48f2
Muhid Hassan Risvy <50704245+Risvy@users.noreply.github.com>
2021-02-07 20:01:10 +0600

    Create 24.py

---
+++ Day_24/24.py
@@ -0,0 +1,13 @@
+  
+/*
+https://leetcode.com/problems/reverse-string/
+Tags: String, Easy
+*/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
